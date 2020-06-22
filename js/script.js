e11082a973847b4aff3ab44b739de438c14f2b5f
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-21 23:22:13 -0300

    correcao de bugs

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js"
-import setTimeout from "./modules/setTimeout.js"
 import initMenuMobile from "./modules/menu-mobile.js"
 import initanimanumeros from "./modules/anima-numeros.js"
 
@@ -18,7 +17,6 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
-setTimeout();
 initMenuMobile();
 initanimanumeros();
 
