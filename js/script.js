93eca5ef04e4d2de6fbab5b517bec26b0a427cd0
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-21 22:58:08 -0300

    envio do curso em andamento

---
+++ js/script.js
@@ -0,0 +1,26 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js"
+import setTimeout from "./modules/setTimeout.js"
+import initMenuMobile from "./modules/menu-mobile.js"
+import initanimanumeros from "./modules/anima-numeros.js"
+
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+setTimeout();
+initMenuMobile();
+initanimanumeros();
+
+
+
